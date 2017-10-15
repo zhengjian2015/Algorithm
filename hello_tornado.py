56ac1a1bda063bc6409cda93b37991505fa7fa7e
zhengj <zhengj>
2017-10-15 19:54:07 +0800

    注释1

--- hello_tornado.py
+++ hello_tornado.py
@@ -1,7 +1,15 @@
+'''
+在程序的最顶部，我们导入了一些Tornado模块。
+虽然Tornado还有另外一些有用的模块，但在这个例子中我们必须至少包含这四个模块。
+'''
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+
+'''
+Tornado包括了一个有用的模块（tornado.options）来从命令行中读取设置
+'''
 
 from tornado.options import define,options
 define("port",default=8000,help="run on the given port",type=int)
